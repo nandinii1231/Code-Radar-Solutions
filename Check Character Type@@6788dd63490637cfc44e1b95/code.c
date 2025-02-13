6e80ae8cea0ee14433a50f3fd00a9ff838d4765f
Nandini Sharma <nandini1280.becse24@chitkara.edu.in>
2025-02-13 17:15:38 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -5,7 +5,7 @@ int main(){
  if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
     printf("Vowel");
  }  
- else if (ch>=65 && ch<=90 || ch>=97 && ch><=122){
+ else if ((ch>=65 && ch<=90) || (ch>=97 && ch><=122){
     printf("Consonant");
  }
  else if(ch>=48 && ch><=57){
