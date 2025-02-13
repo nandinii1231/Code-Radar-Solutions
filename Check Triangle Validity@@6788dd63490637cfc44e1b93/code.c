ead4978514a879d2db3bcba455e886c366864f39
Nandini Sharma <nandini1280.becse24@chitkara.edu.in>
2025-02-13 16:49:19 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d",&a,&b,&c);
+    if(a+b>c||a+c>b||c+b>a){
+        printf("Valid");
+    }    
+    else{
+        printf("Invalid");
+    }
+}
