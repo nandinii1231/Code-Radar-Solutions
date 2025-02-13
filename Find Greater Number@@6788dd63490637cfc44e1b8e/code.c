10972fba251b5ce5712b0d88f6f33ded63372c38
Nandini Sharma <nandini1280.becse24@chitkara.edu.in>
2025-02-13 16:21:51 +0530

    Solution to Find Greater Number

--- Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -3,9 +3,9 @@ int mai(){
     int a,b;
     scanf("%d %d",&a,&b);
     if(a<b){
-        printf(a);
+        printf("%d",a);
     }
     else{
-        printf(b);
+        printf("%d",b);
     }
 }
