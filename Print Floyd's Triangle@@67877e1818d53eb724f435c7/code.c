1857dc26839734823a1d7b42200b3d226f378156
Nandini Sharma <nandini1280.becse24@chitkara.edu.in>
2025-03-07 20:22:57 +0530

    Solution to Print Floyd's Triangle

---
+++ Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int i=1;
+    int value=i;
+    while(i<=n){
+        int j=1;
+        printf(value);
+        j=j+1;
+    }
+    value=value+1;
+    i=i+1;
+}
