a2a74beff57104cf6f822c93d4aeecafbc29bb3e
Nandini Sharma <nandini1280.becse24@chitkara.edu.in>
2025-03-11 18:29:57 +0530

    Solution to Print a Square Pattern

---
+++ Print a Square Pattern@@67877e1818d53eb724f435be/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            printf("*");
+            j=j+1;
+        }
+        i=i+1;
+    }
+}
